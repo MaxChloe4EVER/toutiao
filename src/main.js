abef9a3554eaedb06393175f5e2e2711e53483fa
MaxChloe4EVER <g78529824@qq.com>
2021-07-09 11:42:46 +0800

    项目引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 // 加载全局样式
 import './styles/index.less'
+
+// 导入所有组件
+import Vant from 'vant'
+import '../node_modules/vant/lib/index.css'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
